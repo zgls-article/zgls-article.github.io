a9fa0e9f019f70293c3ceeef7181fcb887ce72ee
DaijinGong <gongdaijin@126.com>
2023-09-16 15:55:06 +0800

    react update

--- js/directory-fresh.js
+++ js/directory-fresh.js
@@ -1,11 +1,7 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import React from 'react';
-// import React, { useState } from 'react';
-
 function Catogary(){
-  // let [fileName,setFileName] = React.useState('雪映專集介紹');
-  let fileName = '雪映專集介紹'
+  let [fileName,setFileName] = React.useState('雪映專集介紹');
+  let [folderName,setFolderName] = React.useState('雪映專集介紹');
+  // let fileName = '雪映專集介紹'
   //通知
   function Notification(){
     console.log(notice)
@@ -26,29 +22,35 @@ function Catogary(){
     return(
       <>
         <li className="nav-item dropdown">
-              <a className="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-bs-toggle="dropdown">
-                <i className="mdi mdi-bell-outline"></i>
-                <span className={noticeNew?"count-symbol bg-danger":"count-symbol"}></span> 
-              </a>
-              <div className="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
-                <h6 className="p-3 mb-0">消息</h6>
-                <div className="dropdown-divider"></div>
-                {res}
-              </div>
-            </li>
-            <li className="nav-item d-none d-lg-block full-screen-link">
-            <a className="nav-link">
-              <div className="mdi mdi-fullscreen" id="fullscreen-button">全屏阅读</div>
-            </a>
-      </li>
+          <a className="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-bs-toggle="dropdown">
+            <i className="mdi mdi-bell-outline"></i>
+            <span className={noticeNew?"count-symbol bg-danger":"count-symbol"}></span> 
+          </a>
+          <div className="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
+            <h6 className="p-3 mb-0">消息</h6>
+            <div className="dropdown-divider"></div>
+            {res}
+          </div>
+        </li>
+        <li className="nav-item  d-none d-lg-block ">
+          <a className="nav-link" onClick={()=>{setFileName='search';setFolderName='search'}}>
+            <div className="mdi mdi-magnify">搜索信息</div>
+          </a>
+        </li>
+        <li className="nav-item d-none d-lg-block full-screen-link">
+          <a className="nav-link">
+            <div className="mdi mdi-fullscreen" id="fullscreen-button">全屏阅读</div>
+          </a>
+        </li>
       </>
     )
   }
   ReactDOM.render(<Notification/>,document.querySelector('#navbar-right'))
   function Content(){
+    let height = document.querySelector('#nav').clientHeight
     return(
       <>
-        <iframe id="iframeBody" src={"./source/"+fileName+'.html'} width="100%" height='1000px' style={{verticalAlign:'bottom'}} frameborder="0"></iframe>
+        <iframe id="iframeBody" src={"./source/"+folderName+'/'+fileName+'.html'} width="100%" height={height} style={{verticalAlign:'bottom'}} frameBorder="0"></iframe>
       </>
     )
   }
@@ -61,7 +63,7 @@ function Catogary(){
       if(item2 && item2.name){
        return(<div className={"collapse ui-basic"+index}>
           <ul className="nav flex-column sub-menu">
-            <li className="nav-item"  onClick={()=>setFileName(item2.name)}> <a className="nav-link" href="">{item2.name}</a></li>
+            <li className="nav-item"  onClick={()=>{setFileName(item2.name);setFolderName(item)}}> <a className="nav-link" href="">{item2.name}</a></li>
           </ul>
         </div>)
       }
